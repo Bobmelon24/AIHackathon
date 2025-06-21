49986fb22eab9d30083df4ea49fd93aeaea3b3e1
Bobmelon24 <141355556+Bobmelon24@users.noreply.github.com>
2025-06-21 12:24:51 -0700

    Update newsapitest.py

--- newsapitest.py
+++ newsapitest.py
@@ -3,13 +3,13 @@ import requests
 url = "https://newsapi.org/v2/everything"
 params = {
     "q": "TikTok",
+    "language": "en",
     "sortBy": "publishedAt",
     "apiKey": "3e8305ecc71f418c8ef844ec285bbea8",
-    "pageSize": 5,
+    "pageSize": 100,
 }
 response = requests.get(url, params=params)
 data = response.json()
-
 #print(data)
 
 for article in data["articles"]:
