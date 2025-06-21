b6eb435c2899e98358d4b7aef21dbc29a2566ad7
anonymoose <127755996+therealanonymoose@users.noreply.github.com>
2025-06-21 12:14:53 -0700

    added newsapi

---
+++ newsapitest.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://newsapi.org/v2/everything"
+params = {
+    "q": "TikTok",
+    "sortBy": "publishedAt",
+    "apiKey": "3e8305ecc71f418c8ef844ec285bbea8",
+    "pageSize": 5,
+}
+response = requests.get(url, params=params)
+data = response.json()
+
+#print(data)
+
+for article in data["articles"]:
+    print(f"- {article['title']}")
