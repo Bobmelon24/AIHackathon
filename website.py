afed68c6dd8ac362c69a3ba66e11a72218136a16
Bobmelon24 <141355556+Bobmelon24@users.noreply.github.com>
2025-06-21 15:08:25 -0700

    revertef back to before the encryption

--- website.py
+++ website.py
@@ -23,7 +23,6 @@ def entry_exists(email_entry, emails):
         for entry in emails
     )
 
-
 def save_email(email_entry):
     emails = load_emails()
     if not entry_exists(email_entry, emails):
@@ -33,23 +32,6 @@ def save_email(email_entry):
         return True
     else:
         return False
-
-# Encryption for emails, will fix later
-
-#from email_encryption import load_emails_encrypted, save_emails_encrypted
-#
-#def save_email(entry):
-#    emails = load_emails_encrypted()
-#    if not any(
-#        e["email"].lower() == entry["email"].lower() and
-#        e["category"].lower() == entry["category"].lower()
-#        for e in emails
-#    ):
-#        emails.append(entry)
-#        save_emails_encrypted(emails)
-#        return True
-#    return False
-
 
 # Form input
 with st.form("subscription_form"):
